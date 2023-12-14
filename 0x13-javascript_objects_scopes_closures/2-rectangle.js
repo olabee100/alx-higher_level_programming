ebbeaa329413e853b61acf8327251f4f809873b2
olabee100 <olabayoantonio@gmail.com>
2023-12-15 00:48:23 +0100

    ok

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // Create an empty object if w or h is not a positive integer
+      Object.create(null);
+    }
+  }
+}
+
+module.exports = Rectangle;
+
