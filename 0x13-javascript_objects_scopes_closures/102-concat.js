c20be30a6827df0e748cbedc46a9ecacd4e20a05
olabee100 <olabayoantonio@gmail.com>
2023-12-15 01:16:35 +0100

    ok

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,22 +1,8 @@
 #!/usr/bin/node
+// script that concats 2 files.
 
+const args = process.argv.slice(2);
 const fs = require('fs');
-
-// Ensure three command-line arguments are provided
-if (process.argv.length !== 5) {
-  console.error('Usage: ./102-concat.js fileA fileB fileC');
-  process.exit(1);
-}
-
-// Read the content of the first source file
-const fileAContent = fs.readFileSync(process.argv[2], 'utf-8');
-
-// Read the content of the second source file
-const fileBContent = fs.readFileSync(process.argv[3], 'utf-8');
-
-// Concatenate the content of the two source files
-const concatenatedContent = fileAContent + fileBContent;
-
-// Write the concatenated content to the destination file
-fs.writeFileSync(process.argv[4], concatenatedContent);
-
+const first = fs.readFileSync('./' + args[0]);
+const second = fs.readFileSync('./' + args[1]);
+fs.writeFileSync('./' + args[2], first + second);
