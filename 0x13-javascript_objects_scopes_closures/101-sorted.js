95638c03cf6821754849d1b92ced5c26dfcf3077
olabee100 <olabayoantonio@gmail.com>
2023-12-15 01:19:18 +0100

    v

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,20 +1,13 @@
 #!/usr/bin/node
+// Script imports a dictionary of occurrences by user id
+// and computes a dictionary of user ids by occurrence.
 
-const dict = require('./101-data').dict;
-
-// Create a new dictionary with user ids sorted by occurrence
-const sortedDict = Object.entries(dict).reduce((acc, [userId, occurrences]) => {
-  // If the occurrences is not a key in the new dictionary, initialize it as an empty array
-  if (!acc[occurrences]) {
-    acc[occurrences] = [];
-  }
-
-  // Add the user id to the array corresponding to its occurrences
-  acc[occurrences].push(userId);
-
-  return acc;
-}, {});
-
-// Print the new dictionary
-console.log(sortedDict);
-
+const { dict } = require('./101-data.js');
+const Dictn = {};
+for (const N in dict) {
+    if (Dictn[dict[N]] === undefined) {
+	Dictn[dict[N]] = [];
+    }
+    Dictn[dict[N]].push(N);
+}
+console.log(Dictn);
